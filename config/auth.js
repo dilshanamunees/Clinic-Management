de1d0e056262bcf899d81614d8c4acaf9ffcd426
dilshanamunees <dilshanaibrahim@gmail.com>
2021-07-28 03:17:25 -0700

    some changes

---
+++ config/auth.js
@@ -0,0 +1,18 @@
+module.exports={
+    facebookAuth:{
+    "clientID" : "******",
+    "clientSecret"   : "*********",
+    "callbackURL"  :  "http://localhost:3000/auth/facebook/callback",
+},
+googleAuth:{
+    "clientID" : "***********",
+    "clientSecret"   : "**********",
+    "callbackURL"  :  "http://localhost:3000/auth/google/callback",
+},
+mobileAuth:{
+    "serviceID":"***********",
+    "accountSID":"*********",
+    "authToken":"**************",
+
+}
+}
